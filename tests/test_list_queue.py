01ae31cf8abf26c330330f0200de7c27bb99d2ca
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-02-27 18:18:43 +0200

    test dequeue list queue

--- tests/test_list_queue.py
+++ tests/test_list_queue.py
@@ -8,3 +8,11 @@ def l_queue():
 def test_enqueue_list(l_queue):
     l_queue.enqueue("First")
     l_queue.enqueue("Second")
+
+def test_dequeue_list(l_queue):
+    l_queue.enqueue(1)
+    l_queue.enqueue(2)
+    val = l_queue.dequeue()
+    assert val == 1
+    assert l_queue.size == 1
+    assert l_queue.items == [2]
