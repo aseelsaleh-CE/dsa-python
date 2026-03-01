1c53919632415f1d5cca790acdbdd9554a5f2bd0
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-01 20:42:58 +0200

    fix: initialize LinkedList with head and length

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -8,7 +8,7 @@ class Node:
 class LinkedList:
     def __init__(self) -> None:
         self.head: Optional[Node] = None
-        self._length: int = 0
+        self.length: int = 0
 
     def get_length(self) -> int:
-        return self._length
+        return self.length
