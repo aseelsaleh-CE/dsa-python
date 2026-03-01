0abcf5ed064339469dc4f12e2289a43625af5b66
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-01 02:35:39 +0200

    add  dequeue  method

--- queues/doubly_linked_list_queue.py
+++ queues/doubly_linked_list_queue.py
@@ -3,9 +3,19 @@ from linked_lists.doubly_linked_list import DoublyLinkedList
 T = TypeVar('T')
 
 class DLLQueue(Generic[T]):
-    
+
     def __init__(self) -> None:
-        self._list: DoublyLinkedList[T] = DoublyLinkedList()
+        self.list: DoublyLinkedList[T] = DoublyLinkedList()
+
+    def is_empty(self) -> bool:
+        return self.list.size == 0
 
     def enqueue(self, item: T) -> None:
-        self._list.insert_at_tail(item)
+        self.list.insert_at_tail(item)
+
+    def dequeue(self) -> T:
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        return self.list.remove_at_head() 
+    
+
