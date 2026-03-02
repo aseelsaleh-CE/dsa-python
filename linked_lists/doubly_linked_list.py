1a640b0f1fd7cf9870a43ae1ec8de7b6b0901335
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-02 15:24:29 +0200

    feat: implement insert at head functionality

--- linked_lists/doubly_linked_list.py
+++ linked_lists/doubly_linked_list.py
@@ -4,16 +4,30 @@ class DoublyNode:
     # Represents a node in a doubly linked list with pointers to next and previous nodes.
     def __init__(self, data: Any):
         self.data = data
-        self.next: Optional[DoublyNode] = None
-        self.prev: Optional[DoublyNode] = None
+        self.next = None
+        self.prev = None
 
-    class DoublyLinkedList:
+class DoublyLinkedList:
     
         def __init__(self):
             # Initializes an empty doubly linked list.
-            self.head: Optional[DoublyNode] = None
-            self.tail: Optional[DoublyNode] = None
-            self.size: int = 0
+            self.head = None
+            self.tail = None
+            self.size = 0
+
         def __len__(self) -> int:
         # Returns the number of elements in the list.
             return self.size
+        
+        def insert_at_head(self, data) -> None:
+        # Adds a new element at the beginning of the list.
+            new_node = DoublyNode(data)
+            if self.head is None: 
+                self.head = new_node
+                self.tail = new_node
+    
+            else:
+                new_node.next = self.head
+                self.head.prev = new_node 
+                self.head = new_node
+            self.size += 1
