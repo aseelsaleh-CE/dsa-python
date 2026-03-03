5bf7fcf49a4d412fa802256b23777719eb2a3394
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-03 19:25:53 +0200

    test:peek method to view top item without removal

--- tests/test_list_stack.py
+++ tests/test_list_stack.py
@@ -4,10 +4,16 @@ def test_push():
     s = ListStack()
     s.push(1)
     assert s.size() == 1
-    
+
 def test_pop_single_element():
     s = ListStack()
     s.push('A')
     popped_value = s.pop()
     assert popped_value == 'A'
     assert s.is_empty() is True
+
+def test_peek_functionality():
+    s = ListStack()
+    s.push('Python')
+    assert s.peek() == 'Python'
+    assert s.size() == 1
