b5f44c7f79b53e36a6812e46f2e2c370661df33a
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-03 23:15:10 +0200

    feat: initialize circular linked list and basic structure

--- src/linked_lists/circular_linked_list.py
+++ src/linked_lists/circular_linked_list.py
@@ -1,24 +1,15 @@
+from typing import Any, Optional
+
 class Node:
-    def __init__(self, data):
-        # Initialize a node with data and next pointer
+    def __init__(self, data: Any) -> None:
         self.data = data
-        self.next = None
-
+        self.next: Optional['Node'] = None
+        
 class CircularLinkedList:
-    def __init__(self):
-        # Initialize an empty circular linked list with head pointer pointing to None
-        self.head = None
+    def __init__(self) -> None:
+        self.head: Optional[Node] = None
+        self.tail: Optional[Node] = None
+        self.size = 0
 
-        # Append a new node with data to the end of the circular linked list
-    def append(self, data):
-        new_node = Node(data)
-
-        if not self.head:
-            new_node.next = new_node
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next != self.head:
-                current = current.next
-            current.next = new_node
-            new_node.next = self.head
+    def is_empty(self) -> bool:
+        return self.head is None
