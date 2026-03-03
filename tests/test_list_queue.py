7de504599c5ae61af58d3eb95178af2609351c6b
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-03 13:48:53 +0200

    test: initialize ListQueue class with an empty items list

--- tests/test_list_queue.py
+++ tests/test_list_queue.py
@@ -1,38 +1,38 @@
 import pytest
 from src.queues.list_queue import ListQueue  
 
-@pytest.fixture
-def l_queue():
-      return ListQueue()
+def test_queue_operation():
+    l_queue = ListQueue()
+   
 
-def test_enqueue_list(l_queue):
-    l_queue.enqueue("First")
-    l_queue.enqueue("Second")
+# def test_enqueue_list(l_queue):
+#     l_queue.enqueue("First")
+#     l_queue.enqueue("Second")
 
-def test_dequeue_list(l_queue):
-    l_queue.enqueue(1)
-    l_queue.enqueue(2)
-    val = l_queue.dequeue()
-    assert val == 1
-    assert l_queue.size == 1
-    assert l_queue.items == [2]
-
-
-def test_clear_list(l_queue):
-    l_queue.enqueue(100)
-    l_queue.clear()
-    assert l_queue.is_empty() is True
-    assert len(l_queue.items) == 0
-
-def test_empty_exception(l_queue):
-    with pytest.raises(Exception) as excinfo:
-        l_queue.dequeue()
-    assert "Queue is empty" in str(excinfo.value)
+# def test_dequeue_list(l_queue):
+#     l_queue.enqueue(1)
+#     l_queue.enqueue(2)
+#     val = l_queue.dequeue()
+#     assert val == 1
+#     assert l_queue.size == 1
+#     assert l_queue.items == [2]
 
 
-def test_front_method(l_queue):
-    l_queue.enqueue("A")
-    l_queue.enqueue("B")
+# def test_clear_list(l_queue):
+#     l_queue.enqueue(100)
+#     l_queue.clear()
+#     assert l_queue.is_empty() is True
+#     assert len(l_queue.items) == 0
+
+# def test_empty_exception(l_queue):
+#     with pytest.raises(Exception) as excinfo:
+#         l_queue.dequeue()
+#     assert "Queue is empty" in str(excinfo.value)
+
+
+# def test_front_method(l_queue):
+#     l_queue.enqueue("A")
+#     l_queue.enqueue("B")
     
-    assert l_queue.front() == "A"
-    assert l_queue.size == 2  
+#     assert l_queue.front() == "A"
+#     assert l_queue.size == 2  
