40bc912ef785f0389c911b4dbcf62664f62af6ae
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-03 22:24:26 +0200

    feat: implement enqueue method (FIFO insertion using append)

--- src/queues/linked_queue_wrapper.py
+++ src/queues/linked_queue_wrapper.py
@@ -6,3 +6,7 @@ class Queue:
 
     def __init__(self) -> None:
         self.list = LinkedList()
+        
+    def enqueue(self, data: Any) -> None:
+        #Adds an element to the rear of the queue.
+        self.list.append(data)
