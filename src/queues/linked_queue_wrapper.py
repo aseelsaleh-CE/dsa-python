0ba7be1615ae14c206cafc4dda7e62ce2e3fbbdc
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-03 22:53:45 +0200

    style: format code spacing and structure according

--- src/queues/linked_queue_wrapper.py
+++ src/queues/linked_queue_wrapper.py
@@ -1,41 +1,40 @@
 from typing import Any
-from src.linked_lists.linked_list import LinkedList 
+from src.linked_lists.linked_list import LinkedList
+
 
 class Queue:
-    #Queue implementation using LinkedList (FIFO).
+    # Queue implementation using LinkedList (FIFO).
 
     def __init__(self) -> None:
         self.list = LinkedList()
-   
+
     def size(self) -> int:
-        #Returns number of elements.
+        # Returns number of elements.
         return self.list.get_length()
 
     def is_empty(self) -> bool:
-        #Checks if the queue is empty
+        # Checks if the queue is empty.
         return self.list.get_length() == 0
-         
+
     def enqueue(self, data: Any) -> None:
-        #Adds an element to the rear of the queue.
+       # Adds an element to the rear of the queue
         self.list.append(data)
 
     def dequeue(self) -> Any:
-        #Removes and returns the front element.
+        # Removes and returns the front element.
         if self.is_empty():
             raise IndexError("Dequeue from empty queue")
         return self.list.remove_at(0)
-    
+
     def peek(self) -> Any:
-        #Returns the front element without removing it.
+        # Returns the front element without removing it.
         if self.is_empty():
             raise IndexError("Peek from empty queue")
         return self.list.head.data
-    
+
     def clear(self) -> None:
-        #Removes all elements.
+        # Removes all elements.
         self.list.clear()
-    
+
     def __str__(self) -> str:
         return str(self.list)
-
-
