39c52deb8ac7f615d1bbf185380d47902e925a7c
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-03 13:50:20 +0200

    feat: add size method to return the number of elements

--- src/queues/list_queue.py
+++ src/queues/list_queue.py
@@ -2,3 +2,7 @@ class ListQueue:
     def __init__(self):
         # Initialize an empty list to store queue elements
         self.items = []
+    
+    def size(self):
+        # Return the total number of items currently in the queue
+        return len(self.items)
