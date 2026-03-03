1621cf67fb9afbae67dc6ed2308c7385fa3df259
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-03 13:48:19 +0200

    feat: initialize ListQueue class with an empty items list

--- src/queues/list_queue.py
+++ src/queues/list_queue.py
@@ -1,33 +1,4 @@
-from typing import List, TypeVar, Generic
-
-T = TypeVar('T')
-
-class ListQueue(Generic[T]):
-    def __init__(self) -> None:
-        self.items: List[T] = []
-    
-    
-    @property
-    def size(self) -> int:
-        return len(self.items)
-    
-    def is_empty(self) -> bool:
-        return len(self.items) == 0
-    
-    def enqueue(self, item: T) -> None:
-        self.items.append(item)
-
-    def dequeue(self) -> T:
-        if self.is_empty():
-            raise Exception("Queue is empty")
-        return self.items.pop(0)
-    
-    def clear(self):
+class ListQueue:
+    def __init__(self):
+        # Initialize an empty list to store queue elements
         self.items = []
-
-    
-    def front(self) -> T:
-        if self.is_empty():
-            raise Exception("Queue is empty")
-        return self.items[0]
-
