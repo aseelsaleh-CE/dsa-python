7e4998e3e1ac7d3a6226d3d4a550ddf0dd342d2b
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-03-03 14:22:53 +0200

    test: dequeue method returning Any data

--- tests/test_linked_queue.py
+++ tests/test_linked_queue.py
@@ -2,7 +2,36 @@ from src.queues.linked_queue import LinkedQueue
 
 def test_initial_state():
     queue = LinkedQueue()
-    assert queue.size == 0
+    assert queue.size() == 0
     assert queue.head is None
     assert queue.tail is None
     print("test_initial_state passed!")
+
+def test_enqueue():
+    queue = LinkedQueue()
+    queue.enqueue(10)
+    assert queue.size() == 1
+    assert queue.is_empty() is False
+    assert queue.head.data == 10
+    assert queue.tail.data == 10
+
+    queue.enqueue(20)
+    assert queue.size() == 2
+    assert queue.head.data == 10
+    assert queue.tail.data == 20
+    print("test_enqueue passed!")
+
+def test_dequeue():
+    queue = LinkedQueue()
+    queue.enqueue("A")
+    queue.enqueue("B")
+    
+    val = queue.dequeue()
+    assert val == "A"
+    assert queue.size() == 1
+    assert queue.head.data == "B"
+    
+    val = queue.dequeue()
+    assert val == "B"
+    assert queue.is_empty() is True
+    print("test_dequeue passed!")
