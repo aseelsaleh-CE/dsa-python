9b974f9b98eb78009a25c99c028c453079b97849
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-04-26 21:30:58 +0200

    feat(linked-list): find middle node using fast and slow pointer

--- src/linked_lists/linked_list.py
+++ src/linked_lists/linked_list.py
@@ -167,3 +167,13 @@ class LinkedList:
             current_index += 1
 
         return None
+    
+    def find_middle(self):
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
