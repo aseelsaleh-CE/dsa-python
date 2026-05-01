906e46c7e046ca521ddd85f2f8e910a5601b218b
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-05-01 23:44:45 +0200

    feat: create binary search tree implementation

--- src/trees/BST.py
+++ src/trees/BST.py
@@ -1,13 +1,13 @@
 class Node:
-    def__init__(self,value):
+    def __init__(self,value):
         self.value = value 
         self.left = None
         self.rigth = None
 
 class BinarySearchTree:
-    def_init__(self):
+    def __init__(self):
         self.root = None
     
-    
+
 
     
