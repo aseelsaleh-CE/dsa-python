06f29d0e34975d98044666460df7d680d04dcb60
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-05-02 00:04:24 +0200

    feat: implement BST insert using recursion

--- src/trees/BST.py
+++ src/trees/BST.py
@@ -8,6 +8,27 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
     
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, current, value):
+        if value < current.value:
+            if current.left is None:
+                current.left = Node(value)
+            else:
+                self._insert_recursive(current.left, value)
+        
+        elif value > current.vlaue:
+            if current.right is None:
+                current.right = Node(value)
+            else :
+                self._insert_recursive(current.right, value)
+
+
+
 
 
     
