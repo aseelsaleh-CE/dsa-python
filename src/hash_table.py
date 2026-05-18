b8df2182e81c062231ea7e9db4439552be2c876e
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-05-18 20:05:19 +0200

    feat: implement hash table insert method

---
+++ src/hash_table.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value 
+        self.next = None
+
+class HashTable:
+    def __init__(self, capacity=8):
+        self.capacity = capacity  # index = hash(key) & (capacity - 1)
+        self.size = 0
+        self.buckets = [None] * self.capacity # [None, None, None, None, None, None, None, None]
+        self.load_factory_threshod = 0.75  # load factor = size / capacity
+    
+   
+    # Converts key into a valid index within the hash table range
+    def _hash(self,key):
+        return hash(key) % self.capacity
+        # total = 0
+        # for char in key:
+        #     total+= ord(char) #Ascii code
+        # return total % self.capacity
+        # return hash(key) & (self.capacity -1) => if capacity 2^power
+    
+    def _resize(self):
+        # Save the old buckets before resizing
+        old_bukets = self.buckets   
+        # Double the capacity to reduce collisions
+        self.capacity *=2
+        self.buckets = [None] * self.capacity
+         # Reset size because elements will be reinserted
+        self.size = 0 
+        
+        # Reinsert all elements into the new buckets (rehashing)
+        for bucket in old_bukets:
+            current = bucket
+          # Traverse linked list in each bucket (handling collisions)
+            while current:
+                self.insert(current.key,current.value)
+                current = current.next
+    
+    def insert(self, key, value):
+        # Get bucket index using hash function
+        index = self._hash(key)
+        # Start from first node in the bucket
+        current = self.buckets[index]
+        # Check if key already exists
+        while current:
+            # If key found, update its value
+            if current.key == key:
+                current.value = value
+                return
+            # Move to next node in linked list
+            current = current.next
+        #create a new node
+        new_node = Node(key, value)
+        #insert new node at the begining 
+        new_node.next = self.buckets[index]
+        #update bucket head to new node
+        self.buckets[index] = new_node
+        #increase number of stored elements
+        self.size += 1
+        
+        if self.size / self.capacity > self.load_factory_threshod:
+            self._resize()
+        
+            
+    
