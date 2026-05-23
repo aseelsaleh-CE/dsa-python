3b8e6000d0c23dd19e9c4508ce5c202617af09fa
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-05-23 20:15:51 +0200

    feat :Add insert operation with heapify up to maintain MinHeap property

--- src/trees/heap.py
+++ src/trees/heap.py
@@ -5,40 +5,13 @@ class Node:
         self.right = None
         self.parent = None
         
-    class MinHeap:
-        def __init__(self):
+class MinHeap:
+    def __init__(self):
             self.root = None
             self.size = 0
-        
-         # GET PARENT NODE (by index)
-    #This function finds the parent node of a given
-    # index in a binary heap by converting the index
-    # into a binary path and traversing the tree 
-    # from the root.It follows the path bit by 
-    # bit (0 = left, 1 = right) until it reaches the 
-    # parent of the target position.
-    
-        def _get_parent(self, index):
-            path = bin(index)[:3]
             
-            current = self.root
             
-            for bit in path[:-1]:
-                if bit == "0":
-                    current = current.left
-                else:
-                    current = current.right
-            
-            return current
-        
-        def get_min(self):
-            if self.root:
-                return self.root.value 
-            else:
-                None
-                
-
-        def insert(self, value):
+    def insert(self, value):
             new_node = Node(value)
             self.size +=1
             
@@ -55,13 +28,40 @@ class Node:
             
             new_node.parent = parent
             self._heapify_up(new_node)
+           
+           
+    def get_min(self):
+            if self.root:
+                return self.root.value 
+            else:
+                return None     
         
-        def _heapify_up(self, node):
+        
+    def _heapify_up(self, node):
             while node.parent and node.value < node.parent.value:
                 node.value,node.parent.value = node.parent.value,node.value
                 node = node.parent
-            
+                 
         
+    def _get_parent(self, index):
+            path = bin(index)[3:]
+            
+            current = self.root
+            
+            for bit in path[:-1]:
+                if bit == "0":
+                    current = current.left
+                else:
+                    current = current.right
+            
+            return current    
+        
+       
+         
+        
+       
+                
+
         
         
         
@@ -75,4 +75,4 @@ class Node:
             
         
             
-                    
+                      
