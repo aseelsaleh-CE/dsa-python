e6dcfec97b56fdfcabd6703892274bae31779973
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-05-23 18:08:54 +0200

    feat: create Node class for heap structure

---
+++ src/trees/heap.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+        
+    class MinHeap:
+        def __init__(self):
+            self.root = None
+            self.size = 0
+            
+   
+   
+            
+        
+            
+                    
