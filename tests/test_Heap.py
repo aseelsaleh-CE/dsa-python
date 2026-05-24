13dad2fd95f7cc9a7eb542821a78a7b57ae302e8
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-05-24 12:29:01 +0200

    test(heap): add insert method tests

--- tests/test_Heap.py
+++ tests/test_Heap.py
@@ -5,5 +5,12 @@ def test_insert_single():
     heap = MinHeap()
     heap.insert(10)
     assert heap.get_min() == 10
+
+def test_multiple_inserts():
+    heap = MinHeap()
+    values = [10, 5, 8, 2, 7]
+    for v in values:
+        heap.insert(v)
+    assert heap.get_min() == 2
     
     
